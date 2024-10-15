338b92ddcb1eb9bacc531c47ab744badc8301254
zilldy <guicardosobarreiro1@gmail.com>
2024-10-15 19:56:14 -0300

    feat: add menu

--- zilldy.com.github.cineminha/src/zilldy/com/github/cineminha/principal/PrincipalComBusca.java
+++ zilldy.com.github.cineminha/src/zilldy/com/github/cineminha/principal/PrincipalComBusca.java
@@ -1,48 +1,14 @@
 package zilldy.com.github.cineminha.principal;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import zilldy.com.github.cineminha.controller.MenuPrincipalComBusca;
 
-import zilldy.com.github.cineminha.controller.ApiOmdb;
-import zilldy.com.github.cineminha.excecao.ErroDeConversaoDeAnoException;
-import zilldy.com.github.cineminha.modelos.Titulo;
+import java.io.IOException;
 
 public class PrincipalComBusca {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        Scanner tc = new Scanner(System.in);
-        String buscaParam = "";
-        List<Titulo> titulos = new ArrayList<>();
-        // controller api
+    public static void main(String[] args) throws IOException {
+        MenuPrincipalComBusca busca = new MenuPrincipalComBusca();
 
+        busca.menuPrincipalComBusca();
 
-        while (!buscaParam.equalsIgnoreCase("sair")) {
-
-            System.out.print("Digite o nome do filme para busca: ");
-            buscaParam = tc.nextLine();
-
-            if (buscaParam.equalsIgnoreCase("sair")) {
-                break;
-            }
-
-            try {
-
-            } catch (NumberFormatException e) {
-                System.out.println("Aconteceu um erro: ");
-                System.out.println(e.getMessage());
-            } catch (
-                    IllegalArgumentException e) {
-                System.out.println("Algum erro de argumento na busca, verifique o endereço");
-            } catch (
-                    ErroDeConversaoDeAnoException e) {
-                System.out.println(e.getMessage());
-            }
-
-        }
-
-        System.out.println(titulos);
-
-        tc.close();
     }
 }
