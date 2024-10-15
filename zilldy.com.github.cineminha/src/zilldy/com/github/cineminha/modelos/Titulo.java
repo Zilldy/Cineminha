53f3464d81716bf9baeb9a20dacdde4071e357e2
zilldy <guicardosobarreiro1@gmail.com>
2024-10-15 16:15:25 -0300

    feat: add FileWriter

--- zilldy.com.github.cineminha/src/zilldy/com/github/cineminha/modelos/Titulo.java
+++ zilldy.com.github.cineminha/src/zilldy/com/github/cineminha/modelos/Titulo.java
@@ -83,7 +83,7 @@ public class Titulo implements Comparable<Titulo> {
 
     @Override
     public String toString() {
-        return "nome: " + nome + ", anoDeLancamento: " + anoDeLancamento + ", duração: " + duracaoEmMinutos;
+        return "(nome: " + nome + ", anoDeLancamento: " + anoDeLancamento + ", duração: " + duracaoEmMinutos + ")";
     }
 
 
