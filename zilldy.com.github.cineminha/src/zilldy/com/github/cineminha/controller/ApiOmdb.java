581bfeae6f6058d7d82353c7b20ffd810204cddd
zilldy <guicardosobarreiro1@gmail.com>
2024-10-15 16:55:30 -0300

    Organizando o PrincipalComBusca

---
+++ zilldy.com.github.cineminha/src/zilldy/com/github/cineminha/controller/ApiOmdb.java
@@ -0,0 +1,51 @@
+package zilldy.com.github.cineminha.controller;
+
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import zilldy.com.github.cineminha.modelos.Titulo;
+import zilldy.com.github.cineminha.modelos.TituloOmdb;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ApiOmdb {
+
+    String busca = "";
+    String chave = "570fbd0f";
+    String endereco = "https://www.omdbapi.com/?t=" + busca.replace(" ", "+") + "&apikey=" + chave;
+    Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).setPrettyPrinting().create();
+    List<Titulo> titulos = new ArrayList<>();
+
+    public void buscaApi(String buscaParam) {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endereco)).build();
+        HttpResponse<String> response = null;
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        String json = response.body();
+        TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+        Titulo meuTitulo = new Titulo(meuTituloOmdb);
+        titulos.add(meuTitulo);
+
+        System.out.println("Título adicionado com sucesso!");
+    }
+
+    public void escreveJSON() throws IOException {
+        FileWriter escrita = new FileWriter("titulos.json");
+        escrita.write(gson.toJson(titulos));
+        escrita.close();
+    }
+
+
+}
