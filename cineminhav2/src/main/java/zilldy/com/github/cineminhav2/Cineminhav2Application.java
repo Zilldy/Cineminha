95e3965336e6ced75a164f111774a95ad914180b
zilldy <guicardosobarreiro1@gmail.com>
2024-10-29 13:09:39 -0300

    feat: Add DadosSerie, ConverteDados, IConverteDados e convertendo o json em objeto java

--- cineminhav2/src/main/java/zilldy/com/github/cineminhav2/Cineminhav2Application.java
+++ cineminhav2/src/main/java/zilldy/com/github/cineminhav2/Cineminhav2Application.java
@@ -3,7 +3,9 @@ package zilldy.com.github.cineminhav2;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zilldy.com.github.cineminhav2.model.DadosSerie;
 import zilldy.com.github.cineminhav2.service.ConsumoAPI;
+import zilldy.com.github.cineminhav2.service.ConverteDados;
 
 @SpringBootApplication
 public class Cineminhav2Application implements CommandLineRunner {
@@ -15,7 +17,9 @@ public class Cineminhav2Application implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		var api = new ConsumoAPI();
-		var json = api.obterDados("https://www.omdbapi.com/?t=gilmore+girls$Season1&apikey=570fbd0f");
-
+		var json = api.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=570fbd0f");
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
 	}
 }
---
+++ cineminhav2/src/main/java/zilldy/com/github/cineminhav2/model/DadosSerie.java
@@ -0,0 +1,10 @@
+package zilldy.com.github.cineminhav2.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosSerie(@JsonAlias("Title") String titulo,
+                         @JsonAlias("totalSeasons") Integer totalTemporadas,
+                         @JsonAlias("imdbRating") String avalicao) {
+}
