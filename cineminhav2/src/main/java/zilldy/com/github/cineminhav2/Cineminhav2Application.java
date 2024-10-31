b424da54f9c7046ef9198303f0dfe43d5f265cce
zilldy <guicardosobarreiro1@gmail.com>
2024-10-31 13:01:24 -0300

    feat: Modelando DadosEpisodios

--- cineminhav2/src/main/java/zilldy/com/github/cineminhav2/Cineminhav2Application.java
+++ cineminhav2/src/main/java/zilldy/com/github/cineminhav2/Cineminhav2Application.java
@@ -3,6 +3,7 @@ package zilldy.com.github.cineminhav2;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zilldy.com.github.cineminhav2.model.DadosEpisodios;
 import zilldy.com.github.cineminhav2.model.DadosSerie;
 import zilldy.com.github.cineminhav2.service.ConsumoAPI;
 import zilldy.com.github.cineminhav2.service.ConverteDados;
@@ -21,5 +22,8 @@ public class Cineminhav2Application implements CommandLineRunner {
 		ConverteDados conversor = new ConverteDados();
 		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
 		System.out.println(dados);
+		json = api.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=570fbd0f");
+		DadosEpisodios episodios = conversor.obterDados(json, DadosEpisodios.class);
+		System.out.println(episodios);
 	}
 }
